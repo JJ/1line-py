1ae4b6da676f178f4eeeb965c12ecb93d82683f1
JJ Merelo <jjmerelo@gmail.com>
2017-08-03 22:08:38 +0200

    Calcula enlaces cortos

--- utils/get-shorty.py
+++ utils/get-shorty.py
@@ -19,13 +19,16 @@ with open( urls_json ) as data_file:
 
 shortened_links = []
 for u in urls:
-    print(u)
-    payload = json.dumps({'longUrl': u[1]})
-    response = requests.post( "https://www.googleapis.com/urlshortener/v1/url?key="+key,
+#    print(u)
+    if (re.search("amzn.to",u[1])):
+        shortened_links.append( [ u[0], u[1], u[1] ] )
+    else:
+        payload = json.dumps({'longUrl': u[1]})
+        response = requests.post( "https://www.googleapis.com/urlshortener/v1/url?key="+key,
                               data=payload,
                               headers={'Content-type': 'application/json'})
-    my_id = json.loads( response.text )
-    print(my_id)
-    shortened_links.append( [ u[0], u[1], my_id['id'] ] )
+        my_id = json.loads( response.text )
+#        print(my_id)
+        shortened_links.append( [ u[0], u[1], my_id['id'] ] )
 
 print(json.dumps( shortened_links ))
