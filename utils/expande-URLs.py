ddc8ce68e23781070d63bf3cfb11c0c4af1151b8
JJ Merelo <jjmerelo@gmail.com>
2017-08-03 22:45:33 +0200

    Expandiendo el rollo

---
+++ utils/expande-URLs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+import glob
+import json
+
+if len(sys.argv) > 1:
+    dir = sys.argv[1]
+else:
+    dir = "txt/"
+
+if len(sys.argv) > 2:
+    short_urls_json = sys.argv[2]
+else:
+    short_urls_json = "short-links.json"
+
+with open( short_urls_json ) as data_file:
+    short_urls = json.load(data_file)
+
+dict_urls = {}
+
+for u in short_urls:
+    dict_urls[u[1]] = u[2]
+
+ficheros = glob.glob(dir+"/*.md")
+for f in ficheros:
+    file_content = open(f,"r").read()
+    re.sub('\[([^\]]+)\]\((http[^\(]+)\)','\1 -> \2', file_content)
+    for key in dict_urls:
+        file_content = file_content.replace(key, dict_urls[key])
+    print(file_content)
+    f = f.replace('.md','-links.md')
+    
+#    with open(f, "w") as links_file:
+#        links_file.write( file_content )
