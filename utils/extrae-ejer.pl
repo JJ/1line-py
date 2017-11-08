b0d68eaa148c353f3cf07da02a9046cc77459c11
JJ Merelo <jjmerelo@gmail.com>
2017-11-08 11:22:35 +0100

    Reorganiza

--- utils/extrae-ejer.pl
+++ utils/extrae-ejer.pl
@@ -2,21 +2,17 @@
 
 use strict;
 use warnings;
+use v5.14;
 
 use File::Slurper qw(read_text);
 
-use v5.14;
-
 my @mds = glob "../txt/*.md";
-
 for my $file (@mds) {
 
   my $contenido = read_text $file;
-  
   my @ejemplos = ( $contenido =~ /```(.+?)```/sg );
 
   say join("\n",map("<section><pre><code>$_</code></pre></section>\n",@ejemplos));
-
   
 }
   
